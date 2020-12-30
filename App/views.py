becbc8603896aafdcab8c2c5bba18c353323027b
DNA5769 <dennisthomas2002@gmail.com>
2020-12-31 02:02:03 +0300

    Created search functionality in website

--- App/views.py
+++ App/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from mal import AnimeSearch
+
 # Create your views here.
 def index(request):
+  if request.method == 'POST':
+    search = request.POST['search']
+    anime = AnimeSearch(search).results[0]
   return render(request, 'App/index.html')

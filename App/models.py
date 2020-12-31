fc88b1e0a535e7046dd5988e910bee7670e68944
DNA5769 <dennisthomas2002@gmail.com>
2020-12-31 03:09:34 +0300

    Modified Anime Model

--- App/models.py
+++ App/models.py
@@ -4,8 +4,6 @@ from django.db import models
 class Anime(models.Model):
   title = models.CharField(max_length=300)
   image_url = models.CharField(max_length=300)
-  episodes = models.IntegerField()
-  completed_episodes = models.IntegerField(default=0)
   status = models.CharField(max_length=20, default='Planning to Watch', choices=((status, status) for status in ['Watching', 'Completed', 'Planning to Watch']))
   date = models.DateTimeField(auto_now_add=True)
 
--- App/views.py
+++ App/views.py
@@ -9,7 +9,7 @@ def index(request):
     search = request.POST['search']
     a = AnimeSearch(search).results[0]
 
-    anime = models.Anime(title=a.title, image_url=a.image_url, episodes=a.episodes)
+    anime = models.Anime(title=a.title, image_url=a.image_url)
     anime.save()
 
   anime = models.Anime.objects.all()
